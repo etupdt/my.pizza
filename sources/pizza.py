8e3d0f6e44737f2100d20d33056a044b2c5b5f8a
Denis Tavernier <denis-tavernier@wanadoo.fr>
2022-10-23 14:19:22 +0200

    add vulcain pizza

--- sources/pizza.py
+++ sources/pizza.py
@@ -35,5 +35,9 @@ RECIPES = {
     'mediterranea': {
         'ingredients': {'mozzarella': 3, 'pepper': 4, 'onion': 3},
         'price': 11,
+    },
+    'vulcain': {
+        'ingredients': {'gorgonzola': 3, 'pepper': 4, 'onion': 3},
+        'price': 14,
     }
 }
